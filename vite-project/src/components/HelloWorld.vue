321be3304cbff4eaa20818a0349cbfde2cbd23c0
juheeeeeee12 <105181103+juheeeeeee12@users.noreply.github.com>
2023-08-19 20:30:49 +0900

    1번째 커밋

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -18,7 +18,7 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
+  <div>안녕안녕 나는 testgit이야</div>
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
