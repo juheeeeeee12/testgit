bf4860cf210ef39f37b97179e6cbf9c54042231d
juheeeeeee12 <105181103+juheeeeeee12@users.noreply.github.com>
2023-08-19 21:25:39 +0900

    커밋하기

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    테스트깃 음 커밋내용을 추가합니당
+    테스트깃 음 커밋내용을 추가합니당 왼쪽에서 바꿔보실레용
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
